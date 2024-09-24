1dcbd53f4b2bd1c35ce808d4c229b92245bdde8c
ambaqinejad <ambaqinejad@gmail.com>
2024-09-24 18:03:53 +0330

    wip

--- src/main/java/com/example/dto/EmployeeDTO.java
+++ src/main/java/com/example/dto/EmployeeDTO.java
@@ -2,10 +2,7 @@ package com.example.dto;
 
 import com.example.model.Employee;
 import com.example.utils.HibernateUtil;
-import jakarta.persistence.criteria.CriteriaBuilder;
-import jakarta.persistence.criteria.CriteriaDelete;
-import jakarta.persistence.criteria.CriteriaQuery;
-import jakarta.persistence.criteria.Root;
+import jakarta.persistence.criteria.*;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -78,6 +75,34 @@ public class EmployeeDTO {
         return employees;
     }
 
+    public List<Employee> getAllEmployeesGreaterThanSpecificAge(int age) {
+        Session session = this.sessionFactory.openSession();
+        session.beginTransaction();
+        CriteriaBuilder cb = session.getCriteriaBuilder();
+        CriteriaQuery<Employee> cq = cb.createQuery(Employee.class);
+        Root<Employee> root = cq.from(Employee.class);
+        cq.select(root).where(cb.ge(root.get("age"), age));
+        List<Employee> employees = session.createQuery(cq).getResultList();
+        System.out.println(employees);
+        session.getTransaction().commit();
+        session.close();
+        return employees;
+    }
+
+    public long getCountOfAllEmployeesGreaterThanSpecificAge(int age) {
+        Session session = this.sessionFactory.openSession();
+        session.beginTransaction();
+        CriteriaBuilder cb = session.getCriteriaBuilder();
+        CriteriaQuery<Long> cq = cb.createQuery(Long.class);
+        Root<Employee> root = cq.from(Employee.class);
+        cq.select(cb.count(root)).where(cb.ge(root.get("age"), age));
+        long count = session.createQuery(cq).getSingleResult();
+        session.getTransaction().commit();
+        session.close();
+        System.out.println(count + " ---------------------------- ");
+        return count;
+    }
+
     public void deleteEmployeeById(int id) {
         Session session = this.sessionFactory.openSession();
         session.beginTransaction();
@@ -104,8 +129,57 @@ public class EmployeeDTO {
         session.close();
     }
 
-    public void updateUsingCriteriaAPI(int id) {
+    public void updateUsingCriteriaAPI(int id, String name, int age) {
+        Session session = this.sessionFactory.openSession();
+        session.beginTransaction();
+        CriteriaBuilder cb = session.getCriteriaBuilder();
+        CriteriaUpdate<Employee> cu = cb.createCriteriaUpdate(Employee.class);
+        Root<Employee> root = cu.from(Employee.class);
+        cu.where(cb.equal(root.get("id"), id));
+        cu.set(root.get("age"), age);
+        cu.set(root.get("name"), name);
+        session.createQuery(cu).executeUpdate();
+        session.getTransaction().commit();
+        session.close();
+    }
 
+    public void updateUsingPreparedMethod(int id, String name, int age) {
+        Session session = this.sessionFactory.openSession();
+        session.beginTransaction();
+        Employee employee = session.get(Employee.class, id);
+        if (employee != null) {
+            employee.setName(name);
+            employee.setAge(age);
+        } else {
+            System.out.println("Employee not found");
+        }
+        session.merge(employee);
+        session.getTransaction().commit();
+    }
+
+    public void updateUsingHQL(int id, String name, int age) {
+        Session session = this.sessionFactory.openSession();
+        session.beginTransaction();
+        session.createQuery("update Employee e set e.name = :name, e.age = :age where e.id = :id")
+                .setParameter("id", id)
+                .setParameter("name", name)
+                .setParameter("age", age)
+                .executeUpdate();
+        session.getTransaction().commit();
+        session.close();
+    }
+
+    public void updateAgeOfAllEmployeesWithSpecificName(String name, int age) {
+        Session session = this.sessionFactory.openSession();
+        session.beginTransaction();
+        CriteriaBuilder cb = session.getCriteriaBuilder();
+        CriteriaUpdate<Employee> cu = cb.createCriteriaUpdate(Employee.class);
+        Root<Employee> root = cu.from(Employee.class);
+        cu.where(cb.equal(root.get("name"), name));
+        cu.set(root.get("age"), age);
+        session.createQuery(cu).executeUpdate();
+        session.getTransaction().commit();
+        session.close();
     }
 
     public void tearDown() {
--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -14,7 +14,8 @@ public class App {
         EmployeeDTO employeeDTO = new EmployeeDTO();
 
         // CREATE
-        employeeDTO.Create("Hossein", 28);
+        // employeeDTO.Create("Hassan", 29);
+        // employeeDTO.Create("Mohammad", 31);
 
         // READ BY ID
         employeeDTO.getEmployeeById(1);
@@ -22,9 +23,17 @@ public class App {
         // READ ALL
         employeeDTO.getAllEmployeesHQL();
         employeeDTO.getAllEmployeesCriteriaAPI();
+        employeeDTO.getAllEmployeesGreaterThanSpecificAge(30);
+        employeeDTO.getCountOfAllEmployeesGreaterThanSpecificAge(30);
 
         // READ ALL WITH NAME
         employeeDTO.getAllEmployeesWithName("Hossein");
+
+        // UPDATE
+        employeeDTO.updateUsingCriteriaAPI(3, "Ali", 30);
+        employeeDTO.updateUsingPreparedMethod(7, "Hassan", 20);
+        employeeDTO.updateAgeOfAllEmployeesWithSpecificName("Hassan", 19);
+        employeeDTO.updateUsingHQL(3, "Karim", 40);
 
         // DELETE BY ID
         employeeDTO.deleteEmployeeById(1);
